cb61d2c9812789ac60ba021d7017ab33999e7a3a
ermanonunes <36741686+ermanonunes@users.noreply.github.com>
2018-02-23 11:13:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 -<?php
-
-function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
-
-    exit();
-}
-
-Redirect('https://www.google.com/', false);
-
- #-echo "<h1>Openshift Workshop v2.0 - Ermano Nunes =)</h1> ";
- #-echo $_SERVER['SERVER_ADDR'];
+-echo "<h1>Openshift Workshop v2.0 - Ermano Nunes =)</h1> ";
+-echo $_SERVER['SERVER_ADDR'];
  -?>
 
